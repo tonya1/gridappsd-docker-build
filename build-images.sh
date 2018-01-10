568f4963b92bb5772fd394b07c7db1859f83b118
Craig Allwardt <craig.allwardt@pnnl.gov>
2018-01-10 14:25:53 -0800

    Added support for building images using tag.

--- build-images.sh
+++ build-images.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-#sudo docker build -f Dockerfile.zeromq -t gridappsd/zeromq .
-#sudo docker build -f Dockerfile.fncs -t gridappsd/fncs .
-#sudo docker build -f Dockerfile.gridappsd_base -t gridappsd/gridappsd_base .
-#sudo docker build -f Dockerfile.gridappsd_viz -t gridappsd/viz .
-sudo docker build -f Dockerfile.gridappsd -t gridappsd/gridappsd .
+# Note this does not export this and it is not
+# set into the context of the container without passing as an argument
+GRIDAPPSD_TAG=":rc2"
+
+docker build --no-cache \
+  --build-arg GRIDAPPSD_TAG=$GRIDAPPSD_TAG \
+  --network=host \
+  -f Dockerfile.gridappsd_base \
+  -t gridappsd/gridappsd_base$GRIDAPPSD_TAG .
+
+docker build --no-cache \
+  --build-arg GRIDAPPSD_TAG=$GRIDAPPSD_TAG \
+  --network=host \
+  -f Dockerfile.gridappsd \
+  -t gridappsd/gridappsd$GRIDAPPSD_TAG .
+
+docker build --no-cache \
+  --build-arg GRIDAPPSD_TAG=$GRIDAPPSD_TAG \
+  --network=host \
+  -f Dockerfile.gridappsd_viz \
+  -t gridappsd/viz$GRIDAPPSD_TAG .

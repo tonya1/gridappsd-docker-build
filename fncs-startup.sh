97eaa03642ab9c652cc5befa7271345e25c8df9b
Tonya Martin <tonya.martin@pnnl.gov>
2018-02-05 12:30:53 -0800

    added simulation, removed port

--- fncs-startup.sh
+++ fncs-startup.sh
@@ -2,7 +2,17 @@
 
 export PATH=$PATH:/gridappsd/bin
 
-PORT=$1
-CON=$2
+CON=$1
+SIM=$2
+
+# change to the logging directory to capture logfile in the right directory
+# create the simulation directory if it doesn't exist
+cd /tmp/gridappsd/log
+
+if [ ! -d $SIM ]; then
+  mkdir $SIM
+fi
+
+cd $SIM
 
 fncs_broker $CON

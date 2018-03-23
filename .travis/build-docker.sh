8c454929bad645a113a5e05e59e8707269dc28f9
Craig A <craig.allwardt@pnnl.gov>
2018-03-23 13:53:01 -0700

    Added support for building and pushing gridappsd/gridappsd_base:dev image.

---
+++ .travis/build-docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+usage () {
+  /bin/echo "Usage:  $0 -b Build the docker image"
+  /bin/echo "           -p Push image to dockerhub"
+  exit 2
+}
+
+IMAGE="gridappsd/gridappsd_base:dev"
+
+# parse options
+while getopts bp option ; do
+  case $option in
+    b) # Pass gridappsd tag to docker-compose
+      docker build -f "../Dockerfile.gridappsd_base" -t $IMAGE .
+      ;;
+    p) # Pass gridappsd tag to docker-compose
+      docker push $IMAGE
+      ;;
+    *) # Print Usage
+      usage
+      ;;
+  esac
+done
+shift `expr $OPTIND - 1`

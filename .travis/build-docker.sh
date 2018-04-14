782587eb07367b1305a285415caca1c801e3c5b9
Craig A <craig.allwardt@pnnl.gov>
2018-04-13 20:56:17 -0700

    Use the build-docker in .travis directory, remove the one in the root

--- .travis/build-docker.sh
+++ .travis/build-docker.sh
@@ -8,15 +8,29 @@ usage () {
 
 IMAGE="gridappsd/gridappsd_base:dev"
 
+trigger_gridappsd_build() {
+body='{
+"request": {
+"branch":"master"
+}}'
+
+curl -s -X POST \
+   -H "Content-Type: application/json" \
+   -H "Accept: application/json" \
+   -H "Travis-API-Version: 3" \
+   -H "Authorization: token $BUILD_AUTH_TOKEN" \
+   -d "$body" \
+   https://api.travis-ci.org/repo/GRIDAPPSD%2FGOSS-GridAPPS-D/requests
+}
 # parse options
 while getopts bp option ; do
   case $option in
     b) # Pass gridappsd tag to docker-compose
-      # Docker file on travis relative from root.
-      docker build -f "Dockerfile.gridappsd_base" -t $IMAGE .
+      docker build -f Dockerfile.gridappsd_base -t $IMAGE .
       ;;
     p) # Pass gridappsd tag to docker-compose
       docker push $IMAGE
+      trigger_gridappsd_build
       ;;
     *) # Print Usage
       usage
@@ -24,4 +38,3 @@ while getopts bp option ; do
   esac
 done
 shift `expr $OPTIND - 1`
-
